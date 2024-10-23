55055f00298cba6cb76fd3c713b8e870deebf074
muhammad athaillah <muhammad.athaillah@ifca.co.id>
2024-10-23 11:04:33 +0700

    setup shadcn

--- client/src/pages/guest/LoginPage.jsx
+++ client/src/pages/guest/LoginPage.jsx
@@ -1,8 +1,16 @@
+import { Button } from "@/components/ui/button"
+
 const LoginPage = () => {
     return (
-        <div className="bg-slate-400 px-4">
-            <p className="text-yellow-300">test</p>
-            <p>login</p>
+        <div className="login">
+            <div className="card">
+                <div className="left">
+                    <h1>Hello world</h1>
+                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Illo hic aliquid obcaecati officia vero rerum facere officiis consectetur asperiores odio, quas iste aliquam fugit sapiente fugiat totam explicabo incidunt doloremque.</p>
+                    <span>Don't have an account?</span>
+                    <Button></Button>
+                </div>
+            </div>
         </div>
     )
 }
