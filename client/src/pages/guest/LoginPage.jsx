fef08b85d773603577e78ef0faaa9c89fdff6160
muhammad athaillah <muhammad.athaillah@ifca.co.id>
2024-10-23 10:12:37 +0700

    initial commit

---
+++ client/src/pages/guest/LoginPage.jsx
@@ -0,0 +1,10 @@
+const LoginPage = () => {
+    return (
+        <div className="bg-slate-400 px-4">
+            <p className="text-yellow-300">test</p>
+            <p>login</p>
+        </div>
+    )
+}
+
+export default LoginPage
